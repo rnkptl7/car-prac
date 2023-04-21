5366b32dd51bc056f3ac8cb354ec2372575b2cd9
Ronak Patel <ronak.patel@simformsolutions.com>
2023-04-21 19:09:41 +0530

    Create UI

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-container">
+    <div class="card" v-for="car in cars">
+      <div class="card-image">
+        <img :src="car.image" alt="" />
+      </div>
+      <div class="card-details">
+        <p class="text-title">{{ car.name }}</p>
+        <p class="text-body">
+          {{
+            car.description.slice(0, 50) +
+            (car.description.length > 50 ? "..." : "")
+          }}
+        </p>
+      </div>
+      <button class="card-button" v-on:click="getPrice(car.price, car.name)">
+        More info
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cars: {
+      type: Object,
+    },
+    getPrice: Function,
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="navbar">
+    <img class="logo" src="/logo.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
